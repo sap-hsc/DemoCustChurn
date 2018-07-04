3aa4d79537018415a337a56e351b319f786f81ed
Balonno <22906446+Balonno@users.noreply.github.com>
2018-07-04 09:49:24 +0200

    Add HANA Content

---
+++ HANA_Catalog_post/post.sql
@@ -0,0 +1,2 @@
+grant select, execute, update, insert, delete on schema DEMO to _SYS_REPO with grant option;
+grant select, execute, update, insert, delete on schema DEMO to DATAHUB_1 with grant option;
